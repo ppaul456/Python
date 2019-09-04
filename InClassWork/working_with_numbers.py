d91581fd476ee687bde084f1ed5a6ea13aba0017
ppaul456 <ppaul456tw@hotmail.com>
2019-09-03 22:55:11 -0500

    0903InClassWork

---
+++ InClassWork/working_with_numbers.py
@@ -0,0 +1,28 @@
+#Pohsun Chang
+#830911
+#MSITM6341
+#Due Date
+
+
+age = 50
+
+disciples_count = 12
+
+cost_of_coffee = 5
+
+gas_price = 2.40
+
+pi = 3.141592654
+
+radius = 5
+
+circumfreence = 2 * pi * radius
+
+print("circumfreence: " + str (circumfreence)) #float turns to string
+
+
+x = 5+3
+y = 2-7
+z = 5/3
+w = 2**4  #2^4
+print(w)
---
+++ InClassWork/working_with_variable.py
@@ -0,0 +1,18 @@
+message = "hello world"
+print(message)
+#message of myself
+
+message_2 = 'hello world'
+print(message_2)
+
+y = '"Hello World"'
+print(y)
+
+hello_world_message = "Hellooo"
+hello_world_message = hello_world_message + "you are awesome"
+print(hello_world_message.upper())
+# Use Upper case
+print(hello_world_message.lower())
+# Use Lower case
+print(hello_world_message.title())
+# capitalize the first letter
