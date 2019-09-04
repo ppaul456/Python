11d0377bd553ec358e24b12d63d8ba0a65a80faa
ppaul456 <ppaul456tw@hotmail.com>
2019-09-03 22:47:51 -0500

    adding header

---
+++ Assignments/homework_assignment_1/hello_world.py
@@ -0,0 +1,9 @@
+#Pohsun Chang
+#830911
+#MSITM6341
+#09/03/2019
+print ("Hello World!")
+print ("My name is PoHsun Chang\n")
+print ("This is my first Python program!\n")
+print ("Have a nice day.")
+
