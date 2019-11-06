38288db2808f0ab4bd8af1557892b246a22d815f
ppaul456 <ppaul456tw@hotmail.com>
2019-11-05 20:18:03 -0600

    check string numbers

---
+++ Attendance/check_string_numbers.py
@@ -0,0 +1,22 @@
+#Pohsun Chang
+#830911
+#MSITM6341
+#11/05/2019
+
+def check_string_numbers(a, b):
+    a = float(a)
+    b = float(b)
+    if a>b:
+        print ("a is larger than b")
+    elif a<b:
+        print ("b is larger than a")
+    else:
+        print ("They are equal")
+
+check_string_numbers("13","11")
+
+
+
+
+
+
