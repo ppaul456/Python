26370f0b9b4eec47a8fca044b11ac3e938128756
ppaul456 <ppaul456tw@hotmail.com>
2019-11-23 14:36:37 -0600

    HW 9 Class Date

--- Assignments/homework_assignment_9/date_class.py
+++ Assignments/homework_assignment_9/date_class.py
@@ -8,25 +8,24 @@ class Date():             # create a class
     def __init__(self):         # special function in python __init__, self (key word in python) is the object that prestents
         self.day = ""
         self.month = ""
-        self.year = ""
-        self.date ={}          # set self.date as a dic  
+        self.year = ""              
 
     def print_date(self):         # self(special key) need to be first
         print(self.month + " " + str(self.day)+ "," + " " + str(self.year))  # call the properties from class, convert int to str
 
-    def change_date(self):   # self need to be written     
-        self.date["day"] = self.day       # save all Data()'s properties into date dic 
-        self.date["month"] = self.month
-        self.date["year"] = self.year
+    def change_date(self,day,month,year):   # self need to be written, and three properties     
+        self.day = day                      
+        self.month = month
+        self.year = year
 
 my_date = Date()
-
+# assign properties for the Class
 my_date.day = 12
 my_date.month = "June"
 my_date.year = 2019
 
 my_date.print_date()       # call print_date function
 
-my_date.change_date()      # call change_date function
-print(my_date.date)
+my_date.change_date(14,"July",2020)   # call change_date function, assign new properties for the Class
+my_date.print_date()
 
